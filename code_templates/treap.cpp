cd72e77ccc7c1d80c9018f6150c668148d6a8200
Mohammad Samiul Islam <forthright48@gmail.com>
2017-05-19 17:19:55 +0600

    Updated treap.cpp
    Added comments for better understanding

--- code_templates/treap.cpp
+++ code_templates/treap.cpp
@@ -18,7 +18,7 @@ struct Treap {
     /*Resets the Treap. Not complete yet. Need to erase all elements from treap*/
     void clear() {
         size = 0;
-        root = NULL;
+        root = NULL; //When the first element is inserted, that element becomes the root
     }
 
     int cnt (pitem t) {
@@ -32,40 +32,57 @@ struct Treap {
     }
 
     void split ( pitem t, int key, pitem &l, pitem &r ) {
-        if ( !t ) {
+        /** Takes a treap and splits it into two subtree l and r
+            such that, l contains elements smaller to key 
+            and r contains elements bigger or equal than key
+        */
+        if ( !t ) { // If treap is empty there is nothing to split
             l = r = NULL;
         }
-        else if ( key < t->key ) {
+        else if ( key < t->key ) { // If key is smaller than root
+            // So now, we have a treap whose right subtree is bigger than root
+            // but its left subtree has SOME values smaller than key
+            // So hey! Lets SPLIT the left subtree and extract those values which are bigger than key 
+            // and put them to treap->l. That way, our current treap will have all values bigger than 
+            // key
             split( t->l, key, l, t->l );
             r = t;
         }
         else {
+            // Same as above. We split treap->r.
             split( t->r, key, t->r, r );
             l = t;
         }
         upd_cnt (t);
     }
     void insert ( pitem &t, pitem it ) {
-        if ( !t ) {
+        if ( !t ) { // If the treap is empty, make the item root of the tree
             t = it;
             size++;
         }
-        else if ( it->prior > t->prior ) {
-            split( t, it->key, it->l, it->r );
+        else if ( it->prior > t->prior ) { // When item is more important than root
+            // Since it is more important than root, IT should be the root
+            split( t, it->key, it->l, it->r ); // Split the treap and put them as child of item
             t = it;
             size++;
         }
-        else {
-            insert( it->key < t->key? t->l:t->r, it );
+        else  // Item is not more important than root
+            if ( it->key < t->key ) insert( t->l, it ); // If item is smaller than root, than go to left subtree
+            else insert ( t->r, it ); // Else go to right subtree
         }
         upd_cnt (t);
     }
 
     void merge ( pitem &t, pitem l, pitem r ) {
+        /** Merges two treap where l contains values smaller than r 
+            
+            After merging the treap, put it inside pitem t
+        */
         if ( !l || !r ) {
             t = l? l:r;
         }
-        else if ( l->prior > r->prior ){
+        else if ( l->prior > r->prior ){ // Left subtree is more important
+            // 
             merge ( l->r, l->r, r );
             t = l;
         }
@@ -83,7 +100,7 @@ struct Treap {
             delete ( e );
             size--;
         }
-        else {
+        else { // Iterate over the treap until we find the key
             erase( key < t->key? t->l:t->r, key );
         }
         upd_cnt (t);
